1a5fd1d146e666ab64eca85398225bef8b8229f4
vardhan <harshwardhankalse03@gmail.com>
2021-06-04 22:08:30 +0530

    Homepage Headline and cta buttons

---
+++ components/Main.js
@@ -0,0 +1,21 @@
+import styles from "../styles/Main.module.css";
+import Link from "next/link";
+
+const Main = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.main}>
+        <h3>Hi My Name Is,</h3>
+        <h1 className={styles.title}>Harshvardhan</h1>
+        <div className={styles.main_actions}>
+          <Link href="/projects">
+            <button className={styles.main_project_btn}>PROJECTS</button>
+          </Link>
+          <button className={styles.main_resume_btn}>RESUME</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import About from "../components/About";
 import Contact from "../components/Contact";
-import styles from "../styles/Home.module.css";
+import Main from "../components/Main";
 
 export default function Home() {
   return (
@@ -14,10 +14,7 @@ export default function Home() {
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
         ></link>
       </Head>
-      <div className={styles.home}>
-        <h3>Hi My Name Is,</h3>
-        <h1 className="title">Harshvardhan</h1>
-      </div>
+      <Main />
       <About />
       <Contact />
     </>
