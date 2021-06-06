0952a2eed768cdd262f8f0c5c20b7db801ea13a1
vardhan <harshwardhankalse03@gmail.com>
2021-06-06 18:47:18 +0530

    Update: Data

--- public/Data.js
+++ public/Data.js
@@ -1,9 +1,9 @@
 export const Data = [
   {
     id: 1,
-    name: "e-commerse Website",
+    name: "e-commerce Website",
     discription:
-      "E-commerse Website having login logout functionality and add-cart too and created with React-js,Html,css,javascript",
+      "E-commerce Website having login logout functionality and add-to-cart too and created with React-js,Html,css,javascript",
     weblink: `https://mangekyou-amaterasu.netlify.app/`,
     githublink: `https://github.com/vardhan35/ecommerse-store`,
   },
@@ -11,7 +11,7 @@ export const Data = [
     id: 2,
     name: "Movie Details App",
     discription:
-      "Movie Details App where You can serach for any movie and Details about it Created with React-js,Html,css,javascript",
+      "Movie Details App where You can search for any movie and Details about it Created with React-js,Html,css,javascript",
     weblink: `https://movie-details-app.netlify.app/`,
     githublink: `https://github.com/vardhan35/movie-app`,
   },
@@ -19,7 +19,7 @@ export const Data = [
     id: 3,
     name: "Wheather App",
     discription:
-      "Wheather App wher you can Search for any cities wheather created with React-js,Html,css,scss,javascript and configured with webpack ",
+      "Wheather App where you can Search for any cities wheather created with React-js,Html,css,scss,javascript and configured with webpack ",
     webslink: `https://wheather-react-app.netlify.app/`,
     githublink: `https://github.com/vardhan35/wheather-app`,
   },
@@ -27,7 +27,7 @@ export const Data = [
     id: 4,
     name: "Todo-App",
     discription:
-      "Todo App where you can Create update And delete Ypur Todos it's created with React-js,Html,css,scss,javascript and configured with webpack",
+      "Todo App where you can Create update And delete Your Todos it's created with React-js,Html,css,scss,javascript and configured with webpack",
     webslink: `https://todo-context-app.netlify.app/`,
     githublink: `https://github.com/vardhan35/Todo-context`,
   },
